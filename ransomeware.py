475e796eaa3f24a16470ac0bddf499183a520644
Batyrkajan <155561218+Batyrkajan@users.noreply.github.com>
2024-12-04 04:03:51 -0500

    Essential methods for class Ransomware

--- ransomeware.py
+++ ransomeware.py
@@ -14,3 +14,21 @@ import os # For interacting with the operating system (e.g., file paths).
 import sys # For accessing system-specific parameters and functions.
 import base64 # For encoding and decoding data using Base64.
 
+class Ransomware:
+    #  This class represents file encrypting ransomware.
+
+    # Constructor for this class
+    def __init__(self, name):
+        self._name = name
+
+    # Getter for name Property
+    @property
+    def name(self):
+        # Name of the malware. 
+        return self._name
+    
+    # Setter for name Property
+    @name.setter
+    def name(self, new_name):
+        # Set a new name
+        self._name = new_name
